fa264180b7009190533a3fd20ab46b692a6a343d
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-02-07 01:01:55 +0900

    convert vector to matrix

--- code/kadai1.cpp
+++ code/kadai1.cpp
@@ -7,24 +7,44 @@
 std::vector< std::vector<double> >
 load_data(std::string filename)
 {
+    // open file stream
     std::ifstream infile(filename);
     if (!infile) std::cout << "unable to load file" << std::endl;
 
+    // get data from file
     std::string str;
     std::vector< std::vector<double> > data;
     while (getline(infile, str)) {
+        // split data string and convert to double
         std::string::size_type sz;
         double val1 = stod(str, &sz);
         double val2 = stod(str.substr(sz));
+        // store data
         std::vector<double> row;
         row.push_back(val1);
         row.push_back(val2);
         data.push_back(row);
     }
+
+    infile.close();
+
     return data;
 }
 
-void display_data(std::vector< std::vector<double> > data)
+Eigen::MatrixXd
+vector2matrix(std::vector< std::vector<double> > vec)
+{
+    Eigen::MatrixXd mat(vec.size(), vec[0].size());
+    for (int i=0; i<vec.size(); i++) {
+        for (int j=0; j<vec[i].size(); j++) {
+            mat.block(i, j, 1, 1) << vec[i][j];
+        }
+    }
+    return mat;
+}
+
+template<typename T>
+void display_data(T data)
 {
     for (int i=0; i<data.size(); i++) {
         for (int j=0; j<data[i].size(); j++) {
@@ -37,5 +57,7 @@ void display_data(std::vector< std::vector<double> > data)
 int main(int argc, char* argv[]) {
     std::vector< std::vector<double> > data;
     data = load_data("../data/Train1.txt");
-    display_data(data);
+    Eigen::MatrixXd mat;
+    mat = vector2matrix(data);
+    std::cout << mat;
 }
