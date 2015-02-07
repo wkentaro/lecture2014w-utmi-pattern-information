05c46371dc02eed82d82454fc3a26fc85e548792
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-02-08 07:16:06 +0900

    add lms class

--- code/kadai1.lisp
+++ code/kadai1.lisp
@@ -20,19 +20,70 @@
     (incf i))
   data)
 
-(defun get-labels (len val)
+
+
+(defun get-vector (len val)
   (setq ret nil)
   (dotimes (i len) (setq ret (cons val ret)))
+  (setq ret (coerce ret vector))
   ret)
 
-; train data
-(setq xtr1 (load-data "../data/Train1.txt"))
-(setq ytr1 (get-labels (send xtr1 :get-val 'dim0) 0))
-(setq xtr2 (load-data "../data/Train2.txt"))
-(setq ytr2 (get-labels (send xtr2 :get-val 'dim0) 1))
-; test data
-(setq xts1 (load-data "../data/Test1.txt"))
-(setq yts1 (get-labels (send xts1 :get-val 'dim0) 0))
-(setq xts2 (load-data "../data/Test2.txt"))
-(setq yts2 (get-labels (send xts2 :get-val 'dim0) 1))
 
+; (defun get-vector (len val)
+;   (setq ret (make-matrix len 1))
+;   (dotimes (i len) (setf (matrix-row ret i) (list val)))
+;   ret)
+
+
+(defclass lms
+  :super propertied-object
+  :slots (eta iterations w))
+(defmethod lms
+  (:init (_eta _iterations)
+         (setq eta _eta)
+         (setq iterations _iterations))
+  (:fit (X y)
+        (let ((n_data (send X :get-val 'dim0))
+              (dim (send X :get-val 'dim1))
+              X_
+              choice
+              pred
+              err
+              dw)
+          (setq X_ (make-matrix n_data (+ dim 1)))
+          (dotimes (i dim) (setf (matrix-column X_ i) (matrix-column X i)))
+          (setf (matrix-column X_ dim) (get-vector n_data 1))
+          (setq w (coerce (get-vector (+ dim 1) 1) float-vector))
+          (dotimes (i iterations)
+            (setq choice (random n_data))
+            ; (print (matrix-row X_ choice))
+            (setq pred (v. (matrix-row X_ choice) w))
+            (setq err (- (aref y choice 0) pred))
+            ; (print (* eta err))
+            (setq dw (scale (* eta err) (matrix-row X_ choice)))
+            (v+ w dw)
+            )
+          ))
+  ; (:predict)
+  ; (:score)
+  )
+
+
+(defun main ()
+  ; train data
+  (setq xtr1 (load-data "../data/Train1.txt"))
+  (setq ytr1 (get-vector (send xtr1 :get-val 'dim0) 0))
+  (setq xtr2 (load-data "../data/Train2.txt"))
+  (setq ytr2 (get-vector (send xtr2 :get-val 'dim0) 1))
+  ; test data
+  (setq xts1 (load-data "../data/Test1.txt"))
+  (setq yts1 (get-vector (send xts1 :get-val 'dim0) 0))
+  (setq xts2 (load-data "../data/Test2.txt"))
+  (setq yts2 (get-vector (send xts2 :get-val 'dim0) 1))
+  )
+
+
+(main)
+(setq *l* (instance lms :init 0.001 10000))
+(send *l* :fit xtr1 ytr1)
+(print (send *l* :get-val 'w))
