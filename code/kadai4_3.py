63b524a812abdee2030730fe0e4457374733d5c3
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-02-09 14:46:37 +0900

    output value

--- code/kadai4_3.py
+++ code/kadai4_3.py
@@ -17,6 +17,8 @@ __author__ = "www.kentaro.wada@gmail.com (Kentaro Wada)"
 
 def main():
     y_pred = generate_with_probability(probability=[.8, .1, .1])
+    print("==> y_pred = {}".format(y_pred))
+
     now = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
     with open('../output/kadai4_3_pred_{}.txt'.format(now), 'w') as f:
         y_pred = map(str, y_pred)
