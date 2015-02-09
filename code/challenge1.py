04c2bc4a062c919fb82117c0d2e734806dff7c21
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-02-10 03:56:40 +0900

    complete FisherLDA

--- code/challenge1.py
+++ code/challenge1.py
@@ -2,9 +2,11 @@
 # -*- coding: utf-8 -*-
 #
 from __future__ import print_function, division
+import datetime
 
 import numpy as np
 import matplotlib.pyplot as plt
+
 
 def get_iris_dataset():
     data = np.loadtxt('../data/iris.txt')
@@ -19,47 +21,87 @@ class PCA(object):
         self.n_components = n_components
 
     def fit(self, X):
-        dim = X.shape[1]
+        n_components = self.n_components
 
         V = np.cov(X, rowvar=0)
         w, v = np.linalg.eig(V)
 
-        W = np.zeros((dim, self.n_components))
-        for i in np.argsort(w)[::-1][:self.n_components]:
-            W[:, i] = v[:, i]
+        W = np.zeros((X.shape[1], n_components))
+        for i, index in enumerate(np.argsort(w)[::-1][:n_components]):
+            W[:, i] = v[:, index]
         self.W = W
 
     def transform(self, X):
-        X_trans = np.zeros((X.shape[0], self.n_components))
-        for i, xi in enumerate(X):
-            X_trans[i] = np.dot(self.W.T, xi)
-        return X_trans
+        return np.dot(X, self.W)  # for without iteration
 
 
 class FisherLDA(object):
     def __init__(self, n_components):
         self.n_components = n_components
 
-    def fit(self):
-        return self
+    def fit(self, X, y):
+        n_components = self.n_components
+
+        Sw = self.within_class_cov(X, y)
+        Sb = self.between_class_cov(X, y)
+
+        w, v = np.linalg.eig(np.dot(np.linalg.inv(Sw), Sb))
+        W = np.zeros((X.shape[1], n_components))
+        for i, index in enumerate(np.argsort(w)[::-1][:n_components]):
+            W[:, i] = v[:, index]
+        self.W = W
 
     def transform(self, X):
-        return X
+        return np.dot(X, self.W)
+
+    def between_class_cov(self, X, y):
+        mu = np.mean(X, axis=0)
+        class_covs = []
+        for cls in np.unique(y):
+            delta = X[y == cls].mean(axis=0) - mu
+            class_covs.append(np.dot(delta, delta.T))
+        return np.sum(class_covs, axis=0)
+
+    def within_class_cov(self, X, y):
+        return np.sum(
+            [np.cov(X[y==cls], rowvar=0) for cls in np.unique(y)],
+            axis=0)
 
 
 def main():
-    X, _ = get_iris_dataset()
-    # PCA decomposition
+    X, y = get_iris_dataset()
+
+    # PCA
+    ## decomposition
     pca = PCA(n_components=1)
     pca.fit(X)
     X_pca_trans = pca.transform(X)
+    ## plot
+    plt.title('PCA decomposition')
+    for cls in np.unique(y):
+        x = X_pca_trans[y == cls]
+        plt.plot(x, np.zeros(len(x)), label='class{}'.format(cls))
+    plt.legend(loc=2)
+    # plt.show()
+    now = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+    plt.savefig('../output/challenge1_pca_plot_{}.png'.format(now))
+    plt.cla()
+
     # Fisher LDA
-    flda = FisherLDA()
-    flda.fit(X)
+    ## decomposition
+    flda = FisherLDA(n_components=1)
+    flda.fit(X, y)
     X_flda_trans = flda.transform(X)
-    # plot
-    plt.plot(np.zeros(X_pca_trans.shape[0]), X_pca_trans)
-    plt.plot(np.zeros(X_flda_trans.shape[0]), X_flda_trans)
+    ## plot
+    plt.title('FisherLDA decomposition')
+    for cls in np.unique(y):
+        x = X_flda_trans[y==cls]
+        plt.plot(x, np.zeros(len(x)), label='class{}'.format(cls))
+    plt.legend(loc=2)
+    # plt.show()
+    now = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+    plt.savefig('../output/challenge1_flda_plot_{}.png'.format(now))
+    plt.cla()
 
 
 if __name__ == '__main__':
